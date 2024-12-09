cc56317e303afe922deae71a90fd10af50aed588
kurniawan <kurni.aawann19@gmail.com>
2024-12-09 23:56:20 +0700

    Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { UserModule } from './user/user.module';
     }),
     UserModule,
     PrismaModule,
+    ValidationModule.forRoot(true),
   ],
   controllers: [AppController],
   providers: [AppService],
