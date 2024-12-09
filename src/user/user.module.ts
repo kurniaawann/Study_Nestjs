7d34b21cddc05746bb98d605572f15c5ab8485a9
kurniawan <kurni.aawann19@gmail.com>
2024-12-09 23:33:29 +0700

    Global Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { Connection, createConnection } from './connection/connection';
 import { mailService, MailService } from './mail/mail.service';
 import { UserRepository } from './user-repository/user-repository';
@@ -8,7 +7,6 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [UserController],
   providers: [
     UserRepository,
