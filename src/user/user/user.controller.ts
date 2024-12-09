2a74a9881ee66b799aef5b5616e26acc8aa7ce1c
kurniawan <kurni.aawann19@gmail.com>
2024-12-09 13:55:48 +0700

    HTTP Request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Hello, my name is ${firstName} ${lastName}.`;
+  }
+
+  @Get('/:id')
+  getId(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
